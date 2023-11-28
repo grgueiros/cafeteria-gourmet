569a30125e7b03a2d16f22ad3acf130978ee28af
Gabriel Reichardt Gueiros <gabriel.gueiros@zarpsystem.com.br>
2023-11-28 08:38:33 -0300

    add tests and deploy action

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,38 +1,39 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Home", () => {
+test.describe("Cart", () => {
   test.beforeEach(async ({ page }) => {
-    // Go to the starting url before each test.
     await page.goto("https://cafeteria.gabrielgueiros.com.br/");
-    await page.getByRole("button", { name: "Adicionar ao Carrinho" }).first().click();
+    await page
+      .getByRole("button", { name: "Adicionar ao Carrinho" })
+      .first()
+      .click();
   });
 
   test("rendered opened cart", async ({ page }) => {
     const title = page.getByRole("heading", { name: "Meu carrinho" });
     await expect(title).toBeVisible();
   });
-  /* test("has title", async ({ page }) => {
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/cafeteria gourmet/i);
+
+  test("buy product success flow", async ({ page }) => {
+    const total = page.getByText("Total R$ 549,00");
+    const paymentButton = page.getByRole("button", {
+      name: "Efetuar Pagamento",
+    });
+
+    await expect(total).toBeVisible();
+
+    await paymentButton.click();
+
+    await expect(page.getByRole("heading", { name: "Parabéns" })).toBeVisible();
   });
 
-  test("has initial elements", async ({ page }) => {
-    const buttons = await page.locator("li").all();
+  test("remove product from cart", async ({ page }) => {
+    const deleteButton = page.getByRole("button", {
+      name: "Remover",
+    });
 
-    expect(buttons.length).toBe(30);
+    await deleteButton.click();
+
+    await expect(page.getByText("Nenhum produto selecionado")).toBeVisible();
   });
-
-  test("search working properly", async ({ page }) => {
-    const textBox = page.getByRole("textbox");
-    await textBox.fill("cereal");
-    await textBox.press("Enter");
-    await page.waitForResponse(
-      "https://cafeteria.gabrielgueiros.com.br/?search=cereal&_data=root"
-    );
-
-    await expect(async () => {
-      const items = await page.locator("li").all();
-      expect(items).toHaveLength(1);
-    }).toPass();
-  }); */
 });
